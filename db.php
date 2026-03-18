7cb881753689e31fcc6a6e09168ba71a4efd044a
projectwebpp <124865459+projectwebpp@users.noreply.github.com>
2026-03-18 11:05:04 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
-$host = getenv('DB_HOST') ?: 'sql307.infinityfree.com';
-$port = getenv('DB_PORT') ?: '3306';
-$dbname = getenv('DB_NAME') ?: 'if0_38904313_phplinebot';
+$host     = getenv('DB_HOST') ?: 'sql307.infinityfree.com';
+$port     = getenv('DB_PORT') ?: '3306';
+$dbname   = getenv('DB_NAME') ?: 'if0_38904313_phplinebot';
 $username = getenv('DB_USER') ?: 'if0_38904313';
 $password = getenv('DB_PASSWORD') ?: 'RyvwsdTAZ3LMj';
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
     $pdo = new PDO($dsn, $username, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 } catch (PDOException $e) {
